64538c2fe81cce83bdf8439770d8d0a2d1434142
kai-ii <67334933+kai-ii@users.noreply.github.com>
2021-10-13 23:24:40 +0100

    Revert "Initial Commit"
    This reverts commit 610eb4eb68595b943715106948f778bbf4507738.

--- Main/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        int a,b;a=b=2;do System.out.println(b*=++a);while(b%3==0);
-    }
-}
