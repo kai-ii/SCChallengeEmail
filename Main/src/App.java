610eb4eb68595b943715106948f778bbf4507738
kai-ii <67334933+kai-ii@users.noreply.github.com>
2021-10-13 09:05:54 +0100

    Initial Commit

---
+++ Main/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a,b;a=b=2;do System.out.println(b*=++a);while(b%3==0);
+    }
+}
